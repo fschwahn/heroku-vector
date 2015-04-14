46b31faf6d6372714a1c9792f7de78ad526009c9
Fabian Schwahn <fabian.schwahn@gmail.com>
2015-04-14 12:35:09 +0200

    Rewrite heroku engine so it stop oldest dynos first (therefore killing of possibly stuck dynos)

--- lib/heroku_vector/engine/heroku.rb
+++ lib/heroku_vector/engine/heroku.rb
@@ -9,55 +9,70 @@ module HerokuVector::Engine
     attr_accessor :app, :heroku
 
     def initialize(options={})
-      options = { app: HerokuVector.heroku_app_name }.merge(options)
-
-      @app = options[:app]
-      @heroku = ::Heroku::API.new
-    end
-
-    def get_dynos_by_name_cached
-      if @dynos_by_name && @dynos_by_name.expires_at > Time.now
-        return @dynos_by_name.data
-      end
-
-      @dynos_by_name = begin
-        expires_at = Time.now + 60 # 1 min from now
-        CacheEntry.new(get_dynos_by_name, expires_at)
-      rescue
-        nil
-      end
-      @dynos_by_name && @dynos_by_name.data
-    end
-
-    def get_dynos_by_name
-      dyno_types = get_dyno_types
-      dyno_types.index_by {|dyno| dyno["name"] }
-    end
-
-    def get_dyno_types
-      response = heroku.get_dyno_types(app)
-      assert_heroku_api_success(response)
-      response.body
+      @app = options[:app] || HerokuVector.heroku_app_name
+      @heroku = options[:heroku] || ::Heroku::API.new
     end
 
     def count_for_dyno_name(dyno_name)
-      dyno = get_dynos_by_name_cached[dyno_name]
-      return 0 unless dyno
-
-      dyno['quantity'] || 0
+      dynos_for_type(dyno_name).size
     end
 
     def scale_dynos(dyno_name, count)
-      response = heroku.post_ps_scale(app, dyno_name, count)
-      assert_heroku_api_success(response)
-      logger.info "Heroku.scale_dynos(#{dyno_name}, #{count})"
-
-      response.body
+      expire_dyno_cache
+      current_count = count_for_dyno_name(dyno_name)
+      if count > current_count
+        scale_dynos_up(dyno_name, count)
+      elsif count < current_count
+        scale_dynos_down(dyno_name, count, current_count)
+      end
     end
 
-    def assert_heroku_api_success(response)
-      raise 'Invalid Heroku API Response' unless response
-      raise "Error #{response.status} from Heroku API" unless response.status == 200
-    end
+    private
+      # Cache dynos for 1 minute to prevent Heroku API from reaching the rate limit
+      def dynos
+        return @dynos.data if @dynos && @dynos.expires_at > Time.now
+        @dynos = CacheEntry.new(get_dynos_from_api, Time.now + 60)
+        @dynos.data
+      end
+
+      def expire_dyno_cache
+        @dynos = nil
+      end
+
+      def get_dynos_from_api
+        run_heroku_api_command(:get_ps, app)
+      end
+
+      def dynos_for_type(type)
+        dynos.select { |d| d['process'].start_with?(type) }
+      end
+
+      def scale_dynos_up(dyno_name, count)
+        run_heroku_api_command(:post_ps_scale, app, dyno_name, count)
+        logger.info "Scaling #{dyno_name} dynos up to #{count}"
+      end
+
+      # Handle scaling dynos down as a special case so always the oldest dyno will be killed
+      def scale_dynos_down(dyno_name, count, current_count)
+         dynos_for_type(dyno_name).sort_by { |d| d['elapsed'] }.reverse.take(current_count - count).each do |dyno|
+          run_heroku_api_command(:post_ps_stop, app, { 'ps' => dyno['process'] })
+         end
+      end
+
+      def run_heroku_api_command(command, *args)
+        response = heroku.send(command, *args)
+        assert_heroku_api_success(response)
+        logger.debug "Heroku.#{command}(#{args.join(', ')})"
+
+        response.body
+      rescue => e
+        logger.warn "Calling Heroku API failed for command: Heroku.#{command}(#{args.join(', ')}): #{e}"
+        nil
+      end
+
+      def assert_heroku_api_success(response)
+        raise 'Invalid Heroku API Response' unless response
+        raise "Error #{response.status} from Heroku API" unless response.status == 200
+      end
   end
 end
--- test/heroku_vector/engine/heroku_test.rb
+++ test/heroku_vector/engine/heroku_test.rb
@@ -2,8 +2,9 @@ require File.absolute_path File.dirname(__FILE__) + '/../../test_helper'
 
 describe HerokuVector::Engine::Heroku do
   let(:heroku_app_name) { 'app_name' }
-  let(:engine) { HerokuVector::Engine::Heroku.new }
-  before { HerokuVector.stubs(:heroku_app_name => heroku_app_name) }
+  let(:mock_heroku) { mock('heroku_api') }
+  let(:engine) { HerokuVector::Engine::Heroku.new(app: heroku_app_name, heroku: mock_heroku ) }
+  let(:get_ps_response) { [ { "elapsed"=>21366, "process"=>"web.1" }, { "elapsed"=>83813, "process"=>"web.2" }, { "elapsed"=>1234, "process"=>"web.3" }, { "elapsed"=>83813, "process"=>"worker.1" } ] }
 
   describe '#initialize' do
     it 'should set app name from config' do
@@ -15,77 +16,44 @@ describe HerokuVector::Engine::Heroku do
     end
   end
 
-  describe '#get_dyno_types' do
-    it 'should load dyno definitions from Heroku API' do
-      VCR.use_cassette('heroku_dyno_types') do
-        dyno_types = engine.get_dyno_types
-        dynos_by_name = dyno_types.index_by {|dyno| dyno["name"] }
-        assert_equal 1, dynos_by_name['web']['quantity']
-        expected_command = "bin/nginx_wrapper bundle exec rainbows -c ./config/rainbows.rb"
-        assert_equal expected_command, dynos_by_name['web']['command']
+  describe '#count_for_dyno_name' do
+    it 'looks up dyno count' do
+      engine.expects(:run_heroku_api_command).with(:get_ps, heroku_app_name).returns(get_ps_response)
+
+      assert_equal 0, engine.count_for_dyno_name('unknown'), 'should return 0 for unknown dyno'
+      assert_equal 3, engine.count_for_dyno_name('web'), 'should return dyno quantity from dynos set'
+    end
+
+    it 'caches the dyno information for 60 seconds' do
+      engine.expects(:run_heroku_api_command).with(:get_ps, heroku_app_name).returns(get_ps_response).twice
+
+      time = Time.now
+      Timecop.freeze(time) do
+        assert_equal 3, engine.count_for_dyno_name('web')
+      end
+      Timecop.freeze(time+30) do
+        assert_equal 3, engine.count_for_dyno_name('web')
+      end
+      Timecop.freeze(time+61) do
+        assert_equal 3, engine.count_for_dyno_name('web')
       end
     end
   end
 
   describe '#scale_dynos' do
-    it 'should call Heroku API and issue command' do
-      VCR.use_cassette('heroku_scale_dynos') do
-        engine.scale_dynos('web', 1)
-
-      end
-    end
-  end
-
-  describe '#get_dynos_by_name' do
-    it 'should index dyno array by name' do
-      dynos = [{ 'name' => 'web', 'quantity' => 1 }]
-      engine.expects(:get_dyno_types => dynos)
-
-      dynos_by_name = engine.get_dynos_by_name
-      assert_equal 1, dynos_by_name['web']['quantity']
-    end
-  end
-
-  describe '#get_dynos_by_name_cached' do
-    it 'should cache calls to get_dynos_by_name' do
-      value = 'dynos'
-      engine.expects(:get_dynos_by_name => value).once
-
-      time = Time.now
-      Timecop.freeze(time) do
-        assert_equal value, engine.get_dynos_by_name_cached
-        assert_equal value, engine.get_dynos_by_name_cached
-        cache_entry = engine.instance_variable_get(:@dynos_by_name)
-        assert cache_entry
-        assert_equal value, cache_entry.data
-        assert_equal time + 60, cache_entry.expires_at
-      end
+    before do
+      engine.expects(:run_heroku_api_command).with(:get_ps, heroku_app_name).returns(get_ps_response)
     end
 
-    it 'should get new value when cached value expired' do
-      value = 'dynos'
-      engine.expects(:get_dynos_by_name => value).twice
-
-      time = Time.now
-      Timecop.freeze(time) do
-        assert_equal value, engine.get_dynos_by_name_cached
-        assert_equal value, engine.get_dynos_by_name_cached
-      end
-      Timecop.freeze(time+61) do
-        assert_equal value, engine.get_dynos_by_name_cached
-      end
+    it 'does scale up if the count is larger than the current dyno number' do
+      engine.expects(:run_heroku_api_command).with(:post_ps_scale, heroku_app_name, 'web', 4)
+      engine.scale_dynos('web', 4)
     end
 
-    describe '#count_for_dyno_name' do
-      it 'should lookup dyno count' do
-        dynos = { 
-          'web' => { 'name' => 'web', 'quantity' => 1 }
-        }
-        engine.stubs(:get_dynos_by_name_cached => dynos)
-
-        assert_equal 0, engine.count_for_dyno_name('unknown'), 'should return 0 for unknown dyno'
-        assert_equal 1, engine.count_for_dyno_name('web'), 'should return dyno quantity from dynos set'
-      end
+    it 'scales down oldest dyno first when downscaling' do
+      engine.expects(:run_heroku_api_command).with(:post_ps_stop, heroku_app_name, { 'ps' => 'web.2' })
+      engine.expects(:run_heroku_api_command).with(:post_ps_stop, heroku_app_name, { 'ps' => 'web.1' })
+      engine.scale_dynos('web', 1)
     end
   end
 end
